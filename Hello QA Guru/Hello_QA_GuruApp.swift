093d7cf72f50dc29636c4ca00c2f6a7c9147dd5d
aynuyanzin <nuyanzin@aeroidea.ru>
2024-07-02 16:29:42 +0300

    Initial Commit

---
+++ Hello QA Guru/Hello_QA_GuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_QA_GuruApp.swift
+//  Hello QA Guru
+//
+//  Created by User on 02.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Hello_QA_GuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
